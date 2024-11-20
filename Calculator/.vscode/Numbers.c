a93eb8ac31b50a62e3c91373cc3c1a598cf8dd49
Peter Thomas <thomasp@macstudent.com.au>
2024-11-20 11:41:20 +1100

    Added comments

--- Calculator/.vscode/Numbers.c
+++ Calculator/.vscode/Numbers.c
@@ -3,19 +3,21 @@
 
 int prime_return = 0;
 
-void odd_even(int x);
+void odd_even(int x); //defining the functions in advance
 void prime(int x);
 void square(int x);
 
 int main(void)
 {
-    int x = get_int("What Number, between 1 and 100\n");
-    odd_even(x);
+    int x = get_int("What Number?\n"); //establishing the number
+    odd_even(x); //calling the functions
+    prime(x);
+    square(x);
 }
 
 void odd_even(int x)
 {
-    if (x % 2 == 0)
+    if (x % 2 == 0) //if x modulus 2 is 0
     {
         printf("Number is even\n");
     }
@@ -27,12 +29,12 @@ void odd_even(int x)
 
 void prime(int x)
 {
-      if (x == 0 || x == 1)
+      if (x == 0 || x == 1) //if x is 0 or 1
     prime_return = 1;
 
-  for (int i = 2; i <= x / 2; ++i) 
+  for (int i = 2; i <= x / 2; ++i) //setting the value for modulusing
   {  
-    if (x % i == 0) {
+    if (x % i == 0) { //if x modulus the value is 0 it is divisible
       prime_return = 1;
       break;
     }
@@ -46,9 +48,9 @@ void prime(int x)
 
 void square(int x)
 {
-    for (int i = 1; i <= x; i++)
+    for (int i = 1; i <= x; i++) //setting the value to divide
     {
-        if (x / i == i)
+        if (x / i == i) //if x divided by i is i, it is a square
         {
             printf("The number is a square number\n");
         }
@@ -61,8 +63,8 @@ void square(int x)
 
 void timestables(int x)
 {
-    for (int z = 1; z < 13; z++)
+    for (int z = 1; z < 13; z++) //setting the value multiplying by
     {
-        printf("%d", x); printf(" * %d", z); printf(" = %d", x * z);
+        printf("%d", x); printf(" * %d", z); printf(" = %d", x * z); //stating the x times z equals ...
     }
 }
