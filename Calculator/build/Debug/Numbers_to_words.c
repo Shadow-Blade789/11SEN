d41be8a1098e0ce1f5bbb638da5eea05147408d7
Peter Thomas <thomasp@macstudent.com.au>
2024-11-21 12:56:32 +1100

    made numbers to words

---
+++ Calculator/build/Debug/Numbers_to_words.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    const char* TEXTUAL[13] = 
+{
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+  "eleven",
+  "twelve",
+};  
+
+printf("%s", TEXTUAL[10]);
+
+}
