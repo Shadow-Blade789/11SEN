8fd44cf67fd0f90a0280936afc66122ca4efc56a
Peter Thomas <thomasp@macstudent.com.au>
2024-11-27 12:12:35 +1100

    fixed evrything, works fully

--- Calculator/build/Debug/Numbers.c
+++ Calculator/build/Debug/Numbers.c
@@ -8,6 +8,7 @@ void odd_even(int x); //defining the functions in advance
 void prime(int x);
 void square(int x);
 void timestables(int x);
+void numbers_to_words(int x);
 
 int main(void)
 {
@@ -16,6 +17,7 @@ int main(void)
     prime(x);
     square(x);
     timestables(x);
+    numbers_to_words(x);
 }
 
 void odd_even(int x)
@@ -83,4 +85,33 @@ void timestables(int x)
     {
         printf("%d", x); printf(" * %d", z); printf(" = %d", x * z); printf("\n"); //stating the x times z equals ...
     }
+}
+
+void numbers_to_words(int x)
+{
+    const char* TEXTUAL[101] = 
+    {
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", 
+        "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", 
+        "eighteen", "nineteen", 
+        "twenty", "twenty-one", "twenty-two", "twenty-three", "twenty-four", "twenty-five", 
+        "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine", 
+        "thirty", "thirty-one", "thirty-two", "thirty-three", "thirty-four", "thirty-five", 
+        "thirty-six", "thirty-seven", "thirty-eight", "thirty-nine", 
+        "forty", "forty-one", "forty-two", "forty-three", "forty-four", "forty-five", 
+        "forty-six", "forty-seven", "forty-eight", "forty-nine", 
+        "fifty", "fifty-one", "fifty-two", "fifty-three", "fifty-four", "fifty-five", 
+        "fifty-six", "fifty-seven", "fifty-eight", "fifty-nine", "sixty", "sixty-one", "sixty-two", "sixty-three", "sixty-four", "sixty-five", 
+        "sixty-six", "sixty-seven", "sixty-eight", "sixty-nine", 
+        "seventy", "seventy-one", "seventy-two", "seventy-three", "seventy-four", 
+        "seventy-five", "seventy-six", "seventy-seven", "seventy-eight", "seventy-nine", 
+        "eighty", "eighty-one", "eighty-two", "eighty-three", "eighty-four", "eighty-five", 
+        "eighty-six", "eighty-seven", "eighty-eight", "eighty-nine", 
+        "ninety", "ninety-one", "ninety-two", "ninety-three", "ninety-four", "ninety-five", 
+        "ninety-six", "ninety-seven", "ninety-eight", "ninety-nine", 
+        "one hundred"
+    };
+
+    printf("%s", TEXTUAL[x]);
+
 }
