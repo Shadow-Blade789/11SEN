d173a9e93ab5e50b31b706b9ef22d013d67dff67
Shadow-Blade789 <thomasp@macstudent.com.au>
2025-05-26 01:58:30 +0000

    up to step 3 of Multiple Characters

--- Python/CaveGameInstructionBased/cave.py
+++ Python/CaveGameInstructionBased/cave.py
@@ -1,2 +1,25 @@
 class Cave:
-    pass
+    def __init__(self, cave_name):
+        self.name = cave_name
+        self.description = None
+        self.linked_caves = {}
+    def get_name(self):
+        return self.name
+    def get_description(self):
+        return self.description
+    def set_description(self, cave_description):
+        self.description = cave_description
+    def describe(self):
+        print(self.description)
+    def link_cave(self, cave_to_link, direction):
+        self.linked_caves[direction] = cave_to_link
+    def get_details(self):
+        for direction in self.linked_caves:
+            cave = self.linked_caves[direction]
+            print( "The " + cave.get_name() + " is " + direction)
+    def move(self, direction):
+        if direction in self.linked_caves:
+            return self.linked_caves[direction]
+        else:
+            print("You can't go that way")
+            return self
---
+++ Python/CaveGameInstructionBased/main.py
@@ -0,0 +1,19 @@
+from cave import Cave
+cavern = Cave("Cavern")
+cavern.set_description("A dank and dirty cave ")
+dungeon = Cave("Dungeon")
+dungeon.set_description("A large cave with a rack")
+grotto = Cave("Grotto")
+grotto.set_description("A small cave with ancient graffiti")
+
+cavern.link_cave(dungeon, "east")
+dungeon.link_cave(cavern, "west")
+grotto.link_cave(cavern, "north")
+cavern.link_cave(grotto, "south")
+
+current_cave = cavern          
+while True:		
+    print("\n")         
+    current_cave.get_details()         
+    command = input("> ")    
+    current_cave = current_cave.move(command)  
