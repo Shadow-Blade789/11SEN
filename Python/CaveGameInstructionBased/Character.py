910ec33568352148e38d4db91fee124c4312a985
Peter Thomas <thomasp@macstudent.com.au>
2025-05-29 11:21:36 +1000

    laptop commit

--- Python/CaveGameInstructionBased/Character.py
+++ Python/CaveGameInstructionBased/Character.py
@@ -20,10 +20,6 @@ class Character():
     def fight(self, combat_item):
         print(self.name + " doesn't want to fight with you")
         return True
-        # Fight with this character
-    def fight(self, combat_item):
-        print(self.name + " doesn't want to fight with you")
-        return True
 # Add your code here (first line unindented)
 class Enemy(Character):
     pass
